41951aa5d07d02627e0c0237e816c561141366c3
Achmat Fauzi <achmatfauzi86@gmail.com>
2024-08-26 18:59:53 +0700

    minimum scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -94,7 +94,7 @@ export default defineNuxtConfig({
   app: {
     head:{
       meta:[
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
         {"http-equiv": "Content-Security-Policy", content: "upgrade-insecure-requests"}
       ]
     },
